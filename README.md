b3d67e945c842d33ca247a090013f13c483f0ae7
shivansh <shivansh.m2003@gmail.com>
2024-04-02 23:33:04 +0530

    update4

---
+++ README.md
@@ -0,0 +1,11 @@
+# Visa-Approval
+
+# git commands
+'''bash
+-git add .
+-git commit -m "updated"
+-git push origin main
+'''
+## How to run ?
+conda create -n visa python=3.8 -y
+conda activate visa
---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,66 @@
+import os
+from datetime import date
+
+Database_name="us_visa"       
+collection_name="visa_data"
+mongodb_url_key="mongoodb_url"
+
+pipeline_name : str = "usvisa"
+artifact_name : str = "artifact"
+model_file_name="model.pkl"
+
+target_column="case_status"
+current_year=date.today().year
+preprocessing_object_filename="preprocessing.pkl"
+
+
+file_name : str = "usvisa.csv"
+train_filename : str = "train.csv"
+test_filename :str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
